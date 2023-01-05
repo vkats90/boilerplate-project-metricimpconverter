67ce72bb2e4243bc4a1f4d3d30bc024aa7746df0
vkats90 <mars77@gmail.com>
2023-01-04 20:26:50 -0700

    fix: all tests are passing in FCC

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,7 +7,7 @@ chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
   this.timeout(5000);
-  test("Convert a valid input such as 10L: GET request to /api/convert.", function (done) {
+  test("Convert a valid input such as 10L: GET request to /api/convert.", function () {
     chai
       .request(server)
       .get("/api/convert?input=10L")
@@ -23,40 +23,36 @@ suite("Functional Tests", function () {
             string: "10 liters converts to 2.64172 gallons",
           })
         );
-        done();
       });
   });
-  test("Convert an invalid input such as 32g: GET request to /api/convert.", function (done) {
+  test("Convert an invalid input such as 32g: GET request to /api/convert.", function () {
     chai
       .request(server)
       .get("/api/convert?input=32g")
       .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.equal(res.text, '"invalid unit"');
-        done();
       });
   });
-  test("Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.", function (done) {
+  test("Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.", function () {
     chai
       .request(server)
       .get("/api/convert?input=3/7.2/4kg")
       .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.equal(res.text, '"invalid number"');
-        done();
       });
   });
-  test("Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.", function (done) {
+  test("Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.", function () {
     chai
       .request(server)
       .get("/api/convert?input=3/7.2/4kilomegagram")
       .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.equal(res.text, '"invalid number and unit"');
-        done();
       });
   });
-  test("Convert with no number such as kg: GET request to /api/convert.", function (done) {
+  test("Convert with no number such as kg: GET request to /api/convert.", function () {
     chai
       .request(server)
       .get("/api/convert?input=kg")
@@ -72,7 +68,6 @@ suite("Functional Tests", function () {
             string: "1 kilograms converts to 2.20462 pounds",
           })
         );
-        done();
       });
   });
 });
